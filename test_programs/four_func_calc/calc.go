a2e75aea1d40c0a90c4173ea7c41bd450ba2f6f0
Jakab Zeller <lordandygello@gmail.com>
2021-10-05 11:37:51 +0100

    Added to the grammar + interactive mode + lexer rules
    - Added Statement, Assignment and Clear symbols and wrote their evaluation referrers (05/10/2021 - 11:15:20)
    - Added an interactive mode (05/10/2021 - 11:37:10)
    - Added some lexer rules so that EOL could be matched and used in the grammar (05/10/2021 - 11:37:43)

--- test_programs/four_func_calc/calc.go
+++ test_programs/four_func_calc/calc.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer"
+	"strings"
 )
 
-//var Lexer = lexer.MustSimple([]lexer.Rule{
-//	{"EOL", `[\n\r]+`, nil},
-//	{"EOF", EOF, nil},
-//	{"Char", scanner.Char, nil},
-//	{"Ident", scanner.Ident, nil},
-//	{"Int", scanner.Int, nil},
-//	{"Float", scanner.Float, nil},
-//	{"String", scanner.String, nil},
-//	{"RawString", scanner.RawString, nil},
-//	{"Comment", scanner.Comment, nil},
-//})
+var Lexer = lexer.MustSimple([]lexer.Rule{
+	{"Number", `[+-]?([0-9]*[.])?[0-9]+`, nil},
+	{"Ident", `[a-zA-Z_]\w*`, nil},
+	{"EOL", `[\n\r]+`, nil},
+	// We add punctuation matching to our Lexer as these characters need to be consumed
+	{"Punct", `[-,()*/+%{};&!=:<>]|\[|\]`, nil},
+	{"whitespace", `\s*`, nil},
+})
 
 // Operator describes the type which all operators must be declared as.
 type Operator int
@@ -45,14 +43,13 @@ func (o *Operator) Capture(s []string) error {
 	if !ok {
 		panic(fmt.Sprintf("Unsupported operator: %s", s[0]))
 	}
-
 	return nil
 }
 
 // A Factor can be either a number, variable identifier, or a set of parenthesis containing another Expression. Note
 // that there is no defined way of taking the exponent in this grammar.
 type Factor struct {
-	Number      *float64    `  @(Float|Int)`
+	Number      *float64    `  @Number`
 	Variable    *string     `| @Ident`
 	Parenthesis *Expression `| "(" @@ ")"`
 }
@@ -78,22 +75,98 @@ type OpTerm struct {
 
 // An Expression is a Term followed by none or many OpTerms.
 type Expression struct {
-	Left *Term       `@@`
-	Right []*OpTerm  `@@*`
+	Tokens     []lexer.Token
+	Left *Term      `@@`
+	Right []*OpTerm `@@*`
 }
 
-// An Assignment consists of a variable identifier followed by the declaration+assignment operator followed by an
-// Expression.
-type Assignment struct {
-	Variable *string        `@Ident ":="`
-	Expression *Expression  `@@`
+// A Clear statement is the keyword "clear" followed by a variable identifier.
+type Clear struct {
+	Variable *string `"clear" @Ident`
 }
+
+// An Assignment consists of the string "let", followed by a variable identifier followed by the declaration+assignment
+// operator followed by an Expression.
+type Assignment struct {
+	Tokens     []lexer.Token
+	Variable   *string     `"let" @Ident`
+	Expression *Expression `"=" @@`
+}
+
 
 // A Statement can either be an expression or a variable assignment.
 type Statement struct {
-	Expression *Expression `@@`
-	Assignment *Assignment `| @@`
-	Eol        *string     `(EOL | ";")`
+	Tokens     []lexer.Token
+	Clear      *Clear      `(   @@`
+	Assignment *Assignment `  | @@`
+	Expression *Expression `  | @@ ) EOL`
+}
+
+func (o Operator) String() string {
+	switch o {
+	case Mul:
+		return "*"
+	case Div:
+		return "/"
+	case Sub:
+		return "-"
+	case Add:
+		return "+"
+	}
+	panic("unsupported operator")
+}
+
+func (v *Factor) String() string {
+	if v.Number != nil {
+		return fmt.Sprintf("%g", *v.Number)
+	}
+	if v.Variable != nil {
+		return *v.Variable
+	}
+	return "(" + v.Parenthesis.String() + ")"
+}
+
+func (p *Product) String() string {
+	return fmt.Sprintf("%s %s", p.Operator.String(), p.Factor.String())
+}
+
+func (t *Term) String() string {
+	out := []string{t.Left.String()}
+	for _, r := range t.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
+}
+
+func (o *OpTerm) String() string {
+	return fmt.Sprintf("%s %s", o.Operator, o.Term)
+}
+
+func (e *Expression) String() string {
+	fmt.Printf("%p\n", e)
+	out := []string{e.Left.String()}
+	for _, r := range e.Right {
+		out = append(out, r.String())
+	}
+	return strings.Join(out, " ")
+}
+
+func (c *Clear) String() string {
+	return fmt.Sprintf("clear %s", *(c.Variable))
+}
+
+func (a *Assignment) String() string {
+	return fmt.Sprintf("%s = %s", *(a.Variable), a.Expression.String())
+}
+
+func (s *Statement) String() string {
+	switch {
+	case s.Clear != nil:
+		return s.Clear.String()
+	case s.Assignment != nil:
+		return s.Assignment.String()
+	}
+	return s.Expression.String()
 }
 
 func (o Operator) Eval(l, r float64) float64 {
@@ -142,20 +215,30 @@ func (e *Expression) Eval(ctx Memory) float64 {
 	return l
 }
 
+func (c *Clear) Eval(ctx Memory) {
+	delete(ctx, *(c.Variable))
+}
+
 func (a *Assignment) Eval(ctx Memory) {
 	ctx[*(a.Variable)] = a.Expression.Eval(ctx)
 }
 
-func (s *Statement) Eval(ctx Memory) float64 {
-	if s.Assignment != nil {
+func (s *Statement) Eval(ctx Memory) (float64, *Memory) {
+	switch {
+	case s.Clear != nil:
+		s.Clear.Eval(ctx)
+		return 0, &ctx
+	case s.Assignment != nil:
 		s.Assignment.Eval(ctx)
-		return 0
+		return 0, &ctx
 	}
-	return s.Expression.Eval(ctx)
+	return s.Expression.Eval(ctx), &ctx
 }
 
 func BuildParser(grammar interface{}) *participle.Parser {
 	return participle.MustBuild(grammar,
 		participle.Lexer(Lexer),
+		participle.CaseInsensitive("Ident"),
+		participle.UseLookahead(2),
 	)
 }
--- test_programs/four_func_calc/main.go
+++ test_programs/four_func_calc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/andygello555/gotils/files"
 	"os"
@@ -8,14 +9,39 @@ import (
 
 func main() {
 	parser := BuildParser(&Statement{})
+	memory := make(Memory)
+
 	if len(os.Args) > 1 {
 		sourceFile := os.Args[1]
 		if files.IsFile(sourceFile) {
 			stmt := &Statement{}
-			err := parser.ParseString(sourceFile, "", stmt)
-			fmt.Println(fmt.Sprintf("Error occurred whilst executing \"%s\": %v", sourceFile, err))
+			if err := parser.ParseString(sourceFile, "", stmt); err != nil {
+				fmt.Println(fmt.Sprintf("Error occurred whilst executing \"%s\": %v", sourceFile, err))
+				os.Exit(1)
+			}
+			result, _ := stmt.Eval(memory)
+			fmt.Println(result)
+		}
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("  Interactive mode   ")
+		fmt.Println("---------------------")
+
+		for {
+			fmt.Print("> ")
+			text, _ := reader.ReadString('\n')
+
+			if text == "quit" {
+				break
+			}
+
+			stmt := &Statement{}
+			if err := parser.ParseString("", text, stmt); err != nil {
+				fmt.Println(fmt.Sprintf("Error occurred whilst executing input: %v", err))
+			}
+			result, _ := stmt.Eval(memory)
+			fmt.Println(result)
 		}
 	}
-	// Interactive mode if no source file was specified?
-	os.Exit(1)
+	os.Exit(0)
 }
