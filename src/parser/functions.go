d8bf660ae82177bd91d2a5d6188e3b9a9a46c0bf
Jakab Zeller <lordandygello@gmail.com>
2021-12-08 00:51:30 +0000

    Language: FunctionCall.Eval + updated ex. 7 + updated CallStack
    - Added function calls to example 7 (08/12/2021 - 00:49:12)
    - Added to the Call and Return referrers for the CallStack type. They now copy over the globals and self from/to the old stack frmae (08/12/2021 - 00:50:08)
    - Implemented FunctionCall.Eval (08/12/2021 - 00:50:47)
    - Updated CallStack interface to match changes to Call and Return referrers (08/12/2021 - 00:51:23)

--- src/parser/functions.go
+++ src/parser/functions.go
@@ -5,6 +5,9 @@ import (
 	"reflect"
 )
 
+// MarshalJSON is used for marshalling FunctionDefinitions to JSON strings as they appear in the data.Heap. The returned
+// byte string is in the format:
+//  "function:RAW_JSON_PATH:FUNCTION_DEF_UINTPTR"
 func (f *FunctionDefinition) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf("\"function:%s:%d\"", f.JSONPath.String(0), reflect.ValueOf(f).Pointer())), nil
 } 
